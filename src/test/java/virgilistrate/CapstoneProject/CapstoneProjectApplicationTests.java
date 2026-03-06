96ed346c53f35a959538f9fa1f2453a49cd076e3
virgilistrate <virgilistrate24@gmail.com>
2026-03-06 12:31:51 +0100

    Initial commit

---
+++ src/test/java/virgilistrate/CapstoneProject/CapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package virgilistrate.CapstoneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
